a3eecce16536d95850f40be387a43579889ff10a
DougLifeOfficial <110689348+DougLifeOfficial@users.noreply.github.com>
2023-03-20 16:18:59 +0000

    Local Date Chage

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,28 @@
 package com.linkedin.javacodechallenges;
 
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.Period;
+
+import javax.crypto.spec.GCMParameterSpec;
+
 public class App 
 {
     // Create function to calculate the date that's 
     // 100 days from now
 
     public static void main( String[] args )
+
     {
-        System.out.println("100 days from now is... " 
-            /* add function call */);
+
+        LocalDate today = LocalDate.now();
+        System.out.println("100 days from now is... " +
+             gDate(today));
     }
+
+    public static LocalDate gDate(LocalDate today) {
+         Period hundredDays = Period.ofDays(100);
+         return today.plus(hundredDays);
+    }
+    
 }
